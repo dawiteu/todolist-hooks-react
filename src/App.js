fa9e8efb65cbd0abddbf429d7139b689b04c918a
dawiteu <dawid@tararuj.be>
2021-03-11 13:32:33 +0100

    fin partie 1

--- src/App.js
+++ src/App.js
@@ -5,110 +5,65 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import TacheForm from './component/TacheForm'; 
 import Tache from './component/Tache'; 
 
+
+let compteurTaches = 0; // au debut c'est a zero. 
+
 const App = () => {
+  
   const [taches, setTaches] = useState([]); 
-  let nbTaches = taches.length; 
-      //const [tachesFinies, setTachesFinies] = useState([]);
-      //let nbTacheF = tachesFinies.length;  
-      //const [tacheAfaire, setTachesAfaire] = useState([]); 
-      //let nbTacheA = tacheAfaire.length; 
-
+  let nbTaches = taches.length;
   const handleAdd = (nom) => {
-    const updateTaches = [...taches]; 
-    //const idTache = nbTaches; // id de la tache 
-    const noTache = nom;  // nom de la tache; 
-
-        //const updateTacheAF = [...tacheAfaire]; 
-          //console.log('Tu essaje dajoputer une tache !'+tache); 
-    const tache = {nom: noTache}; // par defaut elle ne sont pas faites ;  
-
-    //updateTaches.push(tache); 
-    //updateTacheAF.push(tache);
-
+    const updateTaches = [...taches];
+    const idTache = compteurTaches+1; // id de la tache 
+    compteurTaches++;
+    const noTache = nom;  // nom de la tache;  
+    const tache = {id: idTache, nom: noTache, fait: false}; // par defaut elle ne sont pas faites ;  
+    updateTaches.push(tache); 
     setTaches(updateTaches);
-    //setTachesAfaire(updateTacheAF);
   }
 
   const handDel = (id) => {
     const updateTaches = [...taches]; 
-    //const updateTachesFinies = [...tachesFinies]; 
-    //const updateTachesAFaire = [...tacheAfaire]; 
-
     let idTache = updateTaches.findIndex((tache) => tache.id === id); 
-    const objTache = updateTaches[idTache]; 
-
-    if(objTache.fait === false){
-        //updateTachesAFaire.splice(idTache, 1);
-        //setTachesAfaire(updateTachesAFaire); 
-    }else{
-      //updateTachesFinies.splice(idTache, 1); 
-      //setTachesFinies(updateTachesFinies); 
-    }
-
     updateTaches.splice(idTache, 1); 
     setTaches(updateTaches);
   }
 
-  const handCheck = (check) => {
-    console.log(check); 
-
-    /*
-    const updateTaches = [...taches]; 
-    const updateTachesFinies = [...tachesFinies]; 
-    const updateTachesAFaire = [...tacheAfaire]; 
-    
-    let idTache = updateTaches.findIndex((tache) => tache.id === id); 
-    let objTache = updateTaches[idTache]; 
-
-    console.log(id);
-
-    console.log(idTache);
-
-
-    if(objTache.fait === false){
-      objTache.fait = true;
-      updateTachesFinies.push(objTache);
-      updateTachesAFaire.splice(idTache, 1);
-    }else{
-      objTache.fait = false;
-      updateTachesAFaire.push(objTache); 
-      updateTachesFinies.splice(idTache, 1); 
-    }   
-
-    console.log(updateTachesAFaire);
-    console.log(updateTachesFinies);
-
-    setTachesAfaire(updateTachesAFaire);
-    setTachesFinies(updateTachesFinies); 
-    */
+  const handCheck = (check, id) => {
+    let updateTaches = [...taches]; 
+    let idOb = updateTaches.findIndex((tache) => tache.id === id);  
+    let objT = updateTaches[idOb];
+    check ? objT.fait = false : objT.fait = true;
+    setTaches(updateTaches);
   }
 
   return (
     <div className="container">
-      
       <div className="row">
         <div className="col-6 d-flex flex-column"> 
             <h5>Tâches TOTAL: {nbTaches}</h5>
-            <h5>Tâches <span className="text-danger">à faire:</span> </h5>
-            <h5>Tâches <span className="text-success">terminées:</span> </h5>
+            <h5>Tâches <span className="text-danger">à faire:</span> 
+            {taches.filter(tache => tache.fait===false).length}
+            </h5>
+            <h5>Tâches <span className="text-success">terminées:</span>  
+            {taches.filter(tache => tache.fait===true).length}
+            </h5>
         </div>
         <div className="col-6 d-flex flex-column justify-content-center">
             <TacheForm onAdd={handleAdd} />
+            <ul className="d-flex m-2 justify-content-center list-unstyled">
+              <li className="active"><button className="btn btn-secondary mx-2">ALL</button></li>
+              <li><button className="btn btn-secondary mx-2">DO</button></li>
+              <li><button className="btn btn-secondary mx-2">TO DO</button></li>
+            </ul>
         </div>
       </div>
       <hr />
       <div className="container">
-
       { 
         nbTaches > 0 ? taches.map( (tache, i) => <Tache key={i} info={tache} onDel={handDel} onCheck={handCheck} /> ) : "Pas de taches pour l'instant"
       }
-      
       </div>
-
-      
-
-      
-
     </div>
   );
 }
--- src/component/Tache.js
+++ src/component/Tache.js
@@ -1,29 +1,13 @@
-import { useState } from 'react';
 import './Tache.css';
 
 const Tache = ({info, onDel, onCheck}) => {
-
-    //let faux = false; 
-    //const [check, setCheck] = useState(false); 
-
-    const checkCheck = (ev) =>{
-        let checked = ev.target.checked;  
-
-        //console.log(checked);
-
-        return onCheck(checked);
-    }
-
     return (
         <div className="card d-inline-block">
             <div className="info">
-                <input type="checkbox" onChange={(ev) => checkCheck(ev) } /> 
-                <button onClick={(ev) => { onDel(info.id)} }>X</button>
+                <button onClick={() => {onCheck(info.fait, info.id) }} className={info.fait ? 'bg-green' : 'bg-orange'}>V</button>
+                <button onClick={() => { onDel(info.id)} }>X</button>
             </div>
-
-            <div className="pid">
-                Tache id: {info.id}
-            </div>
+            <div className="pid">Tache id: {info.id}</div>
             <h5>Description:</h5>
             <p>{info.nom}</p>
         </div>
